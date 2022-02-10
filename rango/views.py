0284e50b4983102717d1ea2f55da45b6da472c87
“lhknora” <2613092L@student.gla.ac.uk>
2022-02-10 22:35:22 +0000

    finished chapter 1,2,3,4,5

---
+++ rango/views.py
@@ -0,0 +1,12 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+
+from django. http import HttpResponse 
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'author':'This tutorial has been put together by Nora'}
+    return render(request, 'rango/about.html', context = context_dict)
